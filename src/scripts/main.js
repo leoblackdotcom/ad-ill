ce12a5caba5ddd1750d114d2f243bc7860f44e38
Mike Heavers <mheavers@gmail.com>
2020-06-03 16:09:57 -0700

    initial commit fonts base html structure and sass layout base js module and gsap timeline initialization
    intro section revealing transform section text animations
    
    need to animate the fish

---
+++ src/scripts/main.js
@@ -0,0 +1,71 @@
+const ps = (function () {
+  //const counter = 0;
+  const screenDims = {};
+  let tl;
+  let sceneConfig = {
+    transform: {
+      sceneDuration: 4,
+    },
+  };
+
+  getScreenDims = function () {
+    screenDims.width = window.innerWidth;
+    screenDims.height = window.innerHeight;
+  };
+
+  onScrollUpdate = (self) => {
+    console.log(
+      "progress:",
+      self.progress.toFixed(3),
+      "direction:",
+      self.direction,
+      "velocity",
+      self.getVelocity()
+    );
+  };
+
+  initTimeline = function () {
+    tl = gsap.timeline({
+      // yes, we can add it to an entire timeline!
+      scrollTrigger: {
+        trigger: "#section-transform",
+        pin: false, // pin the trigger element while active?
+        start: "top top", // when the top of the trigger hits the top of the viewport
+        end: `+=${sceneConfig.transform.sceneDuration * screenDims.height}`, // end after scrolling this distance
+        scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+        snap: {
+          snapTo: "labels", // snap to the closest label in the timeline
+          duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+          delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+          ease: "power1.inOut", // the ease of the snap animation ("power3" by default)
+        },
+        onUpdate: onScrollUpdate,
+      },
+    });
+    tl.from(".transform-title", { autoAlpha: 0, translateY: 20 }, "start")
+      //.from(".transform-sequence-01", { autoAlpha: 0 }, "start")
+      .from(".transform-copy-p.p1", { autoAlpha: 0 }, "l1")
+      .from(".transform-panel", { translateY: "50vh" }, "panelIn")
+      .from(".transform-copy-p.p2", { autoAlpha: 0 }, "l2")
+      .to(".transform-panel", { translateY: "-150vh", delay: 1 }, "panelOut")
+      .from(".transform-copy-p.p3", { autoAlpha: 0 }, "l3")
+      .from(".transform-tools-container", { translateY: "100vh" }, "l3")
+      .to(
+        ".transform-tools-container",
+        { translateY: "-200vh", delay: 1 },
+        "toolsOut"
+      )
+      .addLabel("end");
+  };
+
+  init = function () {
+    getScreenDims();
+    initTimeline();
+  };
+
+  return {
+    init: init,
+  };
+})();
+
+ps.init();
