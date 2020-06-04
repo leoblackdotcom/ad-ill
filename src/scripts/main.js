eae12e9a263e067cca105e0ee1c91a3c035664e1
Mike Heavers <mheavers@gmail.com>
2020-06-04 10:58:17 -0700

    new timeline params and vid introduce null tweens
    need to convert to canvas video

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -32,10 +32,10 @@ const ps = (function () {
         pin: false, // pin the trigger element while active?
         start: "top top", // when the top of the trigger hits the top of the viewport
         end: `+=${sceneConfig.transform.sceneDuration * screenDims.height}`, // end after scrolling this distance
-        scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+        scrub: 1, // smooth scrubbing, e.g. '1' takes 1 second to "catch up" to the scrollbar. `true` is a direct 1:1 between scrollbar and anim
         snap: {
           snapTo: "labels", // snap to the closest label in the timeline
-          duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+          duration: { min: 1, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
           delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
           ease: "power1.inOut", // the ease of the snap animation ("power3" by default)
         },
@@ -45,27 +45,29 @@ const ps = (function () {
     tl.from(".transform-title", { autoAlpha: 0, translateY: 20 }, "start")
       //.from(".transform-sequence-01", { autoAlpha: 0 }, "start")
       .from(".transform-copy-p.p1", { autoAlpha: 0 }, "l1")
-      .from(".transform-panel", { translateY: "50vh" }, "panelIn")
+      .from(".transform-panel-container", { translateY: "50vh" }, "panelIn")
       .from(".transform-copy-p.p2", { autoAlpha: 0 }, "l2")
       .from(".transform-sequence.t2", { autoAlpha: 0 }, "t2")
-      .to(".transform-panel", { translateY: "-150vh" }, "panelOut")
+      .to('.null',{opacity: 0}, "spacer")
+      .to(".transform-panel-container", { translateY: "-200vh" }, "panelOut")
       .from(".transform-copy-p.p3", { autoAlpha: 0 }, "l3")
       .from(".transform-tools-container", { translateY: "100vh" }, "l3")
-      .from(".transform-sequence.t3", { autoAlpha: 0 }, "t3")
-      .from(".transform-sequence.t4", { autoAlpha: 0 }, "toolsOut")
+      .from(".transform-sequence.t3", { autoAlpha: 0 }, "l3")
+      .to('.null',{opacity: 1}, "spacer")
       .to(".transform-tools-container", { translateY: "-200vh" }, "toolsOut")
+      .to(".transform-sequence.t3", { autoAlpha: 0 }, "t4")
+      .from(".transform-sequence.t4", { autoAlpha: 0}, "t4")
       .from(
-        ".transform-masks-panel",
+        ".transform-masks-panel-container",
         {
           translateY: "100vh",
-          onComplete: () => {
-            document.querySelector(".transform-sequence.t4").play();
-          },
         },
         "masksPanelIn"
       )
-      .from( ".transform-copy-p.p4", { autoAlpha: 0, }, "l4" )
-      .to(".transform-masks-panel", { translateY: "-150vh", delay: 1 }, "masksPanelOut")
+      .from( ".transform-copy-p.p4", { autoAlpha: 0, onComplete: () => {
+        document.querySelector(".transform-sequence.t4").play();
+      }, }, "l4" )
+      .to(".transform-masks-panel-container", { translateY: "-150vh" }, "masksPanelOut")
       .addLabel("end");
   };
 
